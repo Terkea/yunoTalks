04138872049bfd3e5ae619fda1389238e4a11937
Terkea <dororo@DESKTOP-O078LOC.localdomain>
2021-07-12 23:29:40 +0100

    inline buttons homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import Background from "../img/5197264.svg";
 import Chat from "../img/Chat.svg"
 
+import {Link} from 'react-router-dom'
+
 const Home = () => {
 	return (
 		<div className="flex h-full">
@@ -11,7 +13,7 @@ const Home = () => {
 			<div className="xl:w-2/5 lg:w-3/5 w-full pl-5 pr-5 bg-gray-100 text-center flex">
 				<div className="m-auto">
 					{/* TEXT */}
-					<img src={Chat} alt="chat" className="aspect-h-1 m-auto max-w-md"/>
+					<img src={Chat} alt="chat" className="aspect-h-1 m-auto max-w-md mb-5 lg:mb-10"/>
 					<div className="pb-10">
 						<div className="text-4xl lg:text-6xl text-primary font-extrabold">
 							It's happening now
@@ -26,17 +28,17 @@ const Home = () => {
 						</div>
 					</div>
 					{/* BUTTONS */}
-					<div className="">
-						<div className="w-full flex items-center justify-center px-10 py-3 border border-transparent
+					<div className="flex">
+						<Link to={'/login'} className="w-1/2 mr-3 ml-3 items-center justify-center px-10 py-3 border border-transparent
 						text-base font-medium rounded-md text-white bg-action hover:bg-actionH
 						md:py-4 md:text-lg md:px-10 mb-3">
 							Login
-						</div>
-						<div className="w-full flex items-center justify-center px-8 py-3 border border-transparent
+						</Link>
+						<Link to={'/register'} className="w-1/2 mr-3 ml-3 items-center justify-center px-8 py-3 border border-transparent
 						text-base font-medium rounded-md text-primary bg-gray-200 hover:bg-gray-300
 						md:py-4 md:text-lg md:px-10 mb-3">
 							Register
-						</div>
+						</Link>
 					</div>
 				</div>
 			</div>
