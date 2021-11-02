a30b7b72ac824f0b130c1deea956b8466763d898
terkea <terkeabt@gmail.com>
2021-11-02 15:19:10 +0000

    wip conversationsProvider.js

--- src/components/ChatsPanel.js
+++ src/components/ChatsPanel.js
@@ -6,87 +6,72 @@ import {SearchChatsContext} from "../providers/searchChatsProvider";
 import UserAvatar from '../img/anonymous_user.png'
 import {searchLastMessage} from "../utils/message";
 import {computeKeys, decrypt, hexToUint8Array, uncompressPrivateKey} from "../utils/e2ee";
-
-
-const getSharedKey = async (publicKey) => {
-	if (localStorage.getItem('key')) {
-		const privateKey = localStorage.getItem('key');
-		return await computeKeys(uncompressPrivateKey(privateKey), hexToUint8Array(publicKey))
-	}
-}
+import {ConversationsContext} from "../providers/conversationsProvider";
 
 
 const ChatsPanel = () => {
 	const {state} = React.useContext(AuthContext)
 	const searchContext = React.useContext(SearchChatsContext)
-	const [profiles, setProfiles] = React.useState([]);
+	const conversationsContext = React.useContext(ConversationsContext)
+	// const [profiles, setProfiles] = React.useState([]);
 
 
 	// GET THE CONVERSATIONS
-	React.useEffect(() => {
-		// check if the profile was loaded
-		if (state.profile?.friends) {
-			// interate through the friend list
-			state.profile.friends.map(async i => {
-				// in case theres a keyword set use it to filter the results
-				if (searchContext.state.keyword && searchContext.state.keyword.length > 0) {
-					setProfiles(profiles.filter(j => j.nickname.includes(searchContext.state.keyword)))
-				} else {
-					const profile = await searchUserId(i);
-					if (profiles.length > 0) {
-						// if the profile is already in the list dont add it once again
-						// eslint-disable-next-line array-callback-return
-						profiles.map(async j => {
-							if (j.nickname !== profile.nickname) {
-								profile.lastMessage = await searchLastMessage({
-									from: profile.nickname,
-									to: state.profile.nickname
-								})
-								profile.sharedKey = getSharedKey(profile.publicKey) || null
-								setProfiles([...profiles, profile])
-							}
-						})
-						//	if the profiles are not set populate the state
-					} else {
-						profile.lastMessage = await searchLastMessage({
-							from: profile.nickname,
-							to: state.profile.nickname
-						})
-						profile.sharedKey = await getSharedKey(profile.publicKey) || null
-						setProfiles([...profiles, profile])
-					}
-				}
-			})
-		}
-		// eslint-disable-next-line
-	}, [state.profile?.friends, searchContext.state.keyword])
+	// React.useEffect(() => {
+	// 	// check if the profile was loaded
+	// 	if (state.profile?.friends) {
+	// 		// interate through the friend list
+	// 		state.profile.friends.map(async i => {
+	// 			// in case theres a keyword set use it to filter the results
+	// 			if (searchContext.state.keyword && searchContext.state.keyword.length > 0) {
+	// 				setProfiles(profiles.filter(j => j.nickname.includes(searchContext.state.keyword)))
+	// 			} else {
+	// 				const profile = await searchUserId(i);
+	// 				if (profiles.length > 0) {
+	// 					// if the profile is already in the list dont add it once again
+	// 					// eslint-disable-next-line array-callback-return
+	// 					profiles.map(async j => {
+	// 						if (j.nickname !== profile.nickname) {
+	// 							profile.lastMessage = await searchLastMessage({
+	// 								from: profile.nickname,
+	// 								to: state.profile.nickname
+	// 							})
+	// 							profile.sharedKey = getSharedKey(profile.publicKey) || null
+	// 							setProfiles([...profiles, profile])
+	// 						}
+	// 					})
+	// 					//	if the profiles are not set populate the state
+	// 				} else {
+	// 					// profile.lastMessage = [{message: "Say hello to your new friend"}]
+	// 					profile.lastMessage = await searchLastMessage({
+	// 						from: profile.nickname,
+	// 						to: state.profile.nickname
+	// 					})
+	// 					profile.sharedKey = await getSharedKey(profile.publicKey) || null
+	// 					setProfiles([...profiles, profile])
+	// 				}
+	// 			}
+	// 		})
+	// 	}
+	// 	// eslint-disable-next-line
+	// }, [state.profile?.friends, searchContext.state.keyword])
 
 
 	if (state.profile.friends) {
 		return (
 			<div className="contacts p-2 flex-1 overflow-y-scroll">
-				{profiles.length > 0 ? profiles.map((i) => {
-						if (i.sharedKey !== null) {
-							return <ChatPreview
-								key={i.nickname}
-								avatar={i.avatar || UserAvatar}
-								name={i.nickname}
-								lastMessage={decrypt(i.lastMessage[0].message,
-									i.sharedKey,
-									hexToUint8Array(i.lastMessage[1]))
-								}
-								timestamp={new Date(i.lastMessage[0].timestamp)}
-								isNewMessage={false}/>
-						} else {
-							return <ChatPreview
-								key={i.nickname}
-								avatar={i.avatar || UserAvatar}
-								name={i.nickname}
-								lastMessage={i.lastMessage[0].message}
-								timestamp={new Date(i.lastMessage[0].timestamp)}
-								isNewMessage={false}/>
-						}
+				{state.profile.friends.length > 0 ?
+					conversationsContext.state.conversations.map(i => {
+						return <ChatPreview
+							key={i.data.initialisationVector}
+							avatar={i.data.avatar || UserAvatar}
+							name={i.data.nickname}
+							lastMessage={"test"}
+							// timestamp={new Date(i.lastMessage[0].timestamp)}
+							timestamp={Date.now()}
+							isNewMessage={false}/>
 					})
+
 					: <p className="ml-4 text-lg">You don't appear to have any friends yet.</p>
 				}
 			</div>
--- src/providers/conversationsProvider.js
+++ src/providers/conversationsProvider.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import {searchConversation} from "../utils/message";
+import {computeKeys, hexToUint8Array, uncompressPrivateKey} from "../utils/e2ee";
 import {auth, firestore} from '../config/firebase'
+import {AuthContext, searchUserId} from "./authProvider";
 
 
 const initialState = {
@@ -11,17 +14,52 @@ const conversationsReducer = (state, action) => {
 		case 'SET_CONVERSATIONS': {
 			return {...state, conversations: action.payload.conversations}
 		}
+		case 'ADD_CONVERSATION': {
+			return {...state, conversations: [...state.conversations, action.payload.conversations]}
+		}
 		default: {
 			throw new Error(`Unhandled action type: ${action.type}`)
 		}
 	}
 }
 
+// ACTIONS
+const getSharedKey = async (publicKey) => {
+	if (localStorage.getItem('key')) {
+		const privateKey = localStorage.getItem('key');
+		return await computeKeys(uncompressPrivateKey(privateKey), hexToUint8Array(publicKey))
+	}
+}
+
+
 const ConversationsContext = React.createContext({initialState})
 
 const ConversationsProvider = ({children}) => {
 	const [state, dispatch] = React.useReducer(conversationsReducer, initialState)
+	const account = React.useContext(AuthContext)
 	const value = {state, dispatch}
+
+
+	React.useEffect(() => {
+		if (account.state.profile.nickname) {
+			firestore.collection("conversation")
+				.where("parties", "array-contains", account.state.profile.nickname)
+				// .get().then(res => console.log(res.id))
+				.onSnapshot((querySnapshot) => {
+					querySnapshot.forEach(async (doc) => {
+						const data = doc.data()
+						const otherProfile = await searchUserId(doc.data().parties.pop(account.state.profile.nickname));
+						data.sharedKey = await getSharedKey(otherProfile.publicKey)
+						data.avatar = await otherProfile.avatar || ""
+						data.nickname = await otherProfile.nickname
+						dispatch({type: "ADD_CONVERSATION", payload: {conversations: {id: doc.id, data: data}}})
+					});
+
+				});
+		}
+	}, [account.state.profile.nickname])
+
+	console.log(state)
 
 	return (
 		<ConversationsContext.Provider value={value}>
