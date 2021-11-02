ea8df733de39ff0f9b7f2e181c8ca793768bb4b2
terkea <terkeabt@gmail.com>
2021-11-02 09:20:07 +0000

    upload private key

--- src/components/UploadPrivateKey.js
+++ src/components/UploadPrivateKey.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import {readQR} from "../utils/qr";
+
 
 const UploadPrivateKey = ({text}) => {
 	const refInput = React.useRef(null)
+	const [message, setMessage] = React.useState('')
+	const [error, setError] = React.useState('')
+
+	const uploadKey = async (file) => {
+		const key = await readQR(file)
+		if(key){
+			setMessage("Private key successfully uploaded")
+			setError("")
+			localStorage.setItem('key', key.data)
+		}else{
+			setError('Could not decode QR code')
+			setMessage("")
+		}
+	}
+
 
 	return (
 		<>
@@ -15,9 +32,13 @@ const UploadPrivateKey = ({text}) => {
 					{text}
 				</label>
 			</div>
-			<input ref={refInput} style={{opacity: 0}}
+			<input ref={refInput}
+			       onChange={(e) => uploadKey(e.target.files[0])}
+			       style={{opacity: 0}}
 			       accept="image/png, image/jpeg" type="file" name="privateKey"
 			/>
+			<p className='text-green-500 mt-2'>{message}</p>
+			<p className='text-red-500 mt-2'>{error}</p>
 		</>
 	)
 }
--- src/utils/qr.js
+++ src/utils/qr.js
@@ -1,9 +1,19 @@
 import QRCode from 'qrcode'
+import QrcodeDecoder from 'qrcode-decoder';
+
 
 export const generateQR = (data) => {
 	return QRCode.toDataURL(data)
 }
 
-export const readQR = () => {
-
+/**
+ *
+ * @param file File datatype
+ * @returns {*} Promise
+ *
+ * generates a local url and pass it to the decodeFromImage
+ */
+export const readQR = (file) => {
+	const qr = new QrcodeDecoder();
+	return qr.decodeFromImage(URL.createObjectURL(file))
 }
