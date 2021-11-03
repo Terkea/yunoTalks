c2e07e79d7cae2661075bbf16637ae939bb9ffc4
terkea <terkeabt@gmail.com>
2021-11-03 06:01:39 +0000

    filter conversations

--- src/components/ChatsPanel.js
+++ src/components/ChatsPanel.js
@@ -14,6 +14,18 @@ const ChatsPanel = () => {
 	const searchContext = React.useContext(SearchChatsContext)
 	const conversationsContext = React.useContext(ConversationsContext)
 
+	React.useEffect(() => {
+		if (searchContext.state.keyword) {
+			conversationsContext.dispatch({
+				type: 'SEARCH_CONVERSATION',
+				payload: {
+					keyword: searchContext.state.keyword,
+					myNickname: state.profile.nickname
+				}
+			})
+		}
+	}, [searchContext.state.keyword])
+
 	if (state.profile.friends) {
 		return (
 			<div className="contacts p-2 flex-1 overflow-y-scroll">
@@ -23,9 +35,9 @@ const ChatsPanel = () => {
 							key={i.data.initialisationVector}
 							avatar={i.otherProfile.avatar || UserAvatar}
 							name={i.otherProfile.nickname}
-							lastMessage={"test"}
+							lastMessage={i.data.conversation.length > 0 ? "" : "Say hi to your new friend"}
 							// timestamp={new Date(i.lastMessage[0].timestamp)}
-							timestamp={Date.now()}
+							timestamp={i.data.conversation.length > 0 ? "" : ""}
 							isNewMessage={false}/>
 					})
 
--- src/providers/conversationsProvider.js
+++ src/providers/conversationsProvider.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import {searchConversation} from "../utils/message";
 import {computeKeys, hexToUint8Array, uncompressPrivateKey} from "../utils/e2ee";
-import {auth, firestore} from '../config/firebase'
+import {firestore} from '../config/firebase'
 import {AuthContext, searchUserId} from "./authProvider";
+import {SearchChatsContext} from "./searchChatsProvider";
 
 
 const initialState = {
@@ -16,6 +16,13 @@ const conversationsReducer = (state, action) => {
 		}
 		case 'ADD_CONVERSATION': {
 			return {...state, conversations: [...state.conversations, action.payload.conversations]}
+		}
+		case 'SEARCH_CONVERSATION': {
+			return {
+				...state, conversations: [...state.conversations
+					.filter(i => i.otherProfile.nickname.toLowerCase().includes(action.payload.keyword.toLowerCase()))
+				]
+			}
 		}
 		default: {
 			throw new Error(`Unhandled action type: ${action.type}`)
@@ -37,33 +44,53 @@ const ConversationsContext = React.createContext({initialState})
 const ConversationsProvider = ({children}) => {
 	const [state, dispatch] = React.useReducer(conversationsReducer, initialState)
 	const account = React.useContext(AuthContext)
+	const searchContext = React.useContext(SearchChatsContext)
 	const value = {state, dispatch}
 
 
 	React.useEffect(() => {
 		if (account.state.profile.nickname) {
-			firestore.collection("conversation")
-				.where("parties", "array-contains", account.state.profile.nickname)
-				// .get().then(res => console.log(res.id))
-				.onSnapshot(async (querySnapshot) => {
-					dispatch({
-						type: "SET_CONVERSATIONS", payload: {
-							conversations: await Promise.all(querySnapshot.docs.map((doc) => {
-								return searchUserId(doc.data().parties.pop(account.state.profile.nickname)).then(r => {
-									return {
-										id: doc.id,
-										otherProfile: r,
-										data: doc.data()
-									}
-								})
-							}))
-						}
-					})
-				});
-		}
-	}, [account.state.profile.nickname])
+			const fetchConversations = () => {
+				firestore.collection("conversation")
+					.where("parties", "array-contains", account.state.profile.nickname)
+					.onSnapshot(async (querySnapshot) => {
+						dispatch({
+							type: "SET_CONVERSATIONS", payload: {
+								conversations: await Promise.all(querySnapshot.docs.map((doc) => {
+									return searchUserId(doc.data().parties.pop(account.state.profile.nickname))
+										.then(r => {
+											const privateKey = localStorage.getItem('key')
+											let sharedKey;
+											try {
+												sharedKey = computeKeys(uncompressPrivateKey(privateKey), hexToUint8Array(r.publicKey))
+											} catch (e) {
+												sharedKey = ""
+											}
+											return {
+												id: doc.id,
+												otherProfile: r,
+												sharedKey,
+												data: doc.data()
+											}
+										})
+								}))
+							}
+						})
+					});
+			}
 
-	console.log(state)
+			try {
+				if (typeof searchContext.state.keyword.length === 'undefined' || searchContext.state.keyword.replaceAll(" ", "").length === 0) {
+					fetchConversations()
+				}
+			} catch (e) {
+
+			}
+		}
+
+
+	}, [account.state.profile.nickname, searchContext.state.keyword])
+
 
 	return (
 		<ConversationsContext.Provider value={value}>
